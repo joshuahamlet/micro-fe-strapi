59888d47b9ace154ab45a4646f0cd2ab842d380e
Joshua Hamlet <jhamlet@tuta.io>
2022-09-16 17:35:55 -0700

    feat: initial commit

---
+++ dist/src/api/cookie/services/cookie.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * cookie service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::cookie.cookie');
